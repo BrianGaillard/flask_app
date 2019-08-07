78cfe93f4c94234eb577426ea04fc87542152b28
Brian Gaillard <b.gaillard647@gmail.com>
2019-08-06 23:14:24 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def homepage():
+	return render_template('home.html')
+
+@app.route('/about')
+def aboutpage():
+	return render_template('about.html', title='About')
+
+@app.route('/contact')
+def contactpage():
+	return render_template('contact.html', title='Contact')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
