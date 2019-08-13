281985d9ca5a4cd0acef947dde0c1ebe778f906a
Brian Gaillard <b.gaillard647@gmail.com>
2019-08-13 10:11:10 -0400

    Updataed form input - scrolling and response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from forms import YearForm
 
 app = Flask(__name__)
@@ -8,6 +8,9 @@ app.config['SECRET_KEY'] = 'e388702a9f1ecf45eb7724f78c68825e'
 @app.route('/', methods=['GET','POST'])
 def homepage():
 	form = YearForm()
+	if form.validate_on_submit():
+		#flash(f'Your year is {form.year.data}', 'success')
+		return render_template('home.html', scroll='op', check=True, form=form)
 	return render_template('home.html', form=form)
 
 @app.route('/about')
